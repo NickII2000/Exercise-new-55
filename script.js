f32930c985e234ac07943b05147bad72842f4673
Nick <Nick_II2000@mail.ru>
2022-05-19 21:40:35 +0600

    3-th commit

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 const boxesQuery = document.querySelectorAll('.box');
 const boxesGet = document.getElementsByClassName('box');
 
+boxesQuery[0].remove();
+boxesGet[0].remove();
+
+
+
 console.log(boxesQuery);
 console.log(boxesGet);
-console.log(document.body.children);
+// console.log(document.body.children);
+
+console.log(Array.from(boxesGet));
