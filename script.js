437d2887a162024cc4c637f037a3a8ac1421ba7c
Nick <Nick_II2000@mail.ru>
2022-05-19 21:30:39 +0600

    2-th commit

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 'use strict';
+const boxesQuery = document.querySelectorAll('.box');
+const boxesGet = document.getElementsByClassName('box');
 
+console.log(boxesQuery);
+console.log(boxesGet);
+console.log(document.body.children);
