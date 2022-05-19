276efab24efb3d64dc29d800916b1359e0148356
Nick <Nick_II2000@mail.ru>
2022-05-19 21:59:53 +0600

    6-th commit

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
 const boxesQuery = document.querySelectorAll('.box');
 const boxesGet = document.getElementsByClassName('box');
 
-boxesQuery[0].remove();
-boxesGet[0].remove();
+boxesQuery.forEach(box => {
+    if (box.matches('.this')) console.log(box);
+});
 
-for (let i = 0; i < 5; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    document.body.append(div);
-    // boxesGet[boxesGet.length] = div; нельзя добавить в коллекцию
-}
+console.log(boxesQuery[0].closest('.wrapper'));
+console.log(boxesQuery[0].closest('.wra'));
 
-console.log(boxesQuery);
-console.log(boxesGet);
-// console.log(document.body.children);
+// boxesQuery[0].remove();
+// boxesGet[0].remove();
 
-console.log(Array.from(boxesGet));
+// for (let i = 0; i < 5; i++) {
+//     const div = document.createElement('div');
+//     div.classList.add('box');
+//     document.body.append(div);
+//     // boxesGet[boxesGet.length] = div; нельзя добавить в коллекцию
+// }
+
+// console.log(boxesQuery);
+// console.log(boxesGet);
+// // console.log(document.body.children);
+
+// console.log(Array.from(boxesGet));
