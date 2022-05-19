761a61a33da142f7312ca381db69aab2d8066ae9
Nick <Nick_II2000@mail.ru>
2022-05-19 21:47:44 +0600

    4-th commit

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const boxesGet = document.getElementsByClassName('box');
 boxesQuery[0].remove();
 boxesGet[0].remove();
 
-
+for (let i = 0; i < 5; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    document.body.append(div);
+}
 
 console.log(boxesQuery);
 console.log(boxesGet);
